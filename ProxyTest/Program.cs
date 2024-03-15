aa1b38db8ec77071aebee09a156252cc33144df5
luisfbcudj94 <alejandroabril98@gmail.com>
2024-03-15 13:11:29 -0500

    loop

--- ProxyTest/Program.cs
+++ ProxyTest/Program.cs
@@ -13,169 +13,177 @@ using System;
 using System.Globalization;
 
 
-string url = "https://albertsons.okta.com/oauth2/ausp6soxrIyPrm8rS2p6/v1/authorize?client_id=0oap6ku01XJqIRdl42p6&response_type=code&scope=openid%20profile%20email%20offline_access&redirect_uri=https://www.safeway.com/bin/safeway/unified/sso/authorize&state=wasteful-stem-rabid-join&nonce=1d5ce452-f925-461a-92a5-c7f22521b11d&prompt=none";
+//string url = "https://albertsons.okta.com/oauth2/ausp6soxrIyPrm8rS2p6/v1/authorize?client_id=0oap6ku01XJqIRdl42p6&response_type=code&scope=openid%20profile%20email%20offline_access&redirect_uri=https://www.safeway.com/bin/safeway/unified/sso/authorize&state=wasteful-stem-rabid-join&nonce=1d5ce452-f925-461a-92a5-c7f22521b11d&prompt=none";
 
 //string url = "https://mail.google.com/mail/u/0/?pli=1#inbox";
 
+// Read txt
+var urlFilePath = "url.txt";
+string[] urls = File.ReadAllLines(urlFilePath);
+
 ChromeOptions options = new ChromeOptions();
-options.AddArgument("--disable-features=IsolateOrigins,site-per-process");
-options.BinaryLocation = @"C:\Users\aleja\Downloads\chrome-win64\chrome.exe";
-options.AddArguments("disable-features=NetworkService");
-options.AddArguments("--disable-web-security");
-options.AddArguments("--allow-running-insecure-content");
-options.AddArguments("--disable-extensions");
-options.AddArguments("--ignore-certificate-errors");
-options.AddArguments("--disable-notifications");
-options.AddArguments("--disable-popup-blocking");
+//options.AddArgument("--disable-features=IsolateOrigins,site-per-process");
+//options.BinaryLocation = @"C:\Users\aleja\Downloads\chrome-win64\chrome.exe";
+//options.AddArguments("disable-features=NetworkService");
+//options.AddArguments("--disable-web-security");
+//options.AddArguments("--allow-running-insecure-content");
+//options.AddArguments("--disable-extensions");
+//options.AddArguments("--ignore-certificate-errors");
+//options.AddArguments("--disable-notifications");
+//options.AddArguments("--disable-popup-blocking");
 
 
 using var driver = new ChromeDriver(options);
 
 String script = "var performance = window.performance || window.mozPerformance || window.msPerformance || window.webkitPerformance || {}; var network = performance.getEntries() || {}; return network;";
-var networkdata = ((IJavaScriptExecutor)driver).ExecuteScript(script);
 
-List<string> requests = new List<string>();
-List<string> responses = new List<string>();
-Dictionary<string, List<JObject>> dataToExcelRequest = new Dictionary<string, List<JObject>>();
-Dictionary<string, List<JObject>> dataToExcelResponse = new Dictionary<string, List<JObject>>();
+//var networkdata = ((IJavaScriptExecutor)driver).ExecuteScript(script);
 
-IDevTools devTools = driver;
-DevToolsSession session = devTools.GetDevToolsSession();
-await session.Domains.Network.EnableNetwork();
-
-bool firstRequest = false;
-string firstRequestId = string.Empty;
-bool stopSession = false;
-
-session.DevToolsEventReceived += (sender, e) =>
+foreach (var url in urls)
 {
-    if (dataToExcelRequest.Count == 0 && dataToExcelResponse.Count == 0)
+    Dictionary<string, List<JObject>> dataToExcelRequest = new Dictionary<string, List<JObject>>();
+    Dictionary<string, List<JObject>> dataToExcelResponse = new Dictionary<string, List<JObject>>();
+
+
+    IDevTools devTools = driver;
+    DevToolsSession session = devTools.GetDevToolsSession();
+    await session.Domains.Network.EnableNetwork();
+
+    bool firstRequest = false;
+    string firstRequestId = string.Empty;
+    bool stopSession = false;
+
+    session.DevToolsEventReceived += (sender, e) =>
     {
-        firstRequest = true;
-    }
-    else
-    {
-        firstRequest = false;
-    }
-
-    if (e.EventName == "requestWillBeSentExtraInfo")
-    {
-        JObject jsonObjectRequest = JObject.Parse(e.EventData.ToString());
-        JObject resultObjectRequest = new JObject();
-        JObject jsonObjectRequestHeaders = JObject.Parse(jsonObjectRequest["headers"].ToString());
-
-        resultObjectRequest["requestId"] = jsonObjectRequest["requestId"];
-        resultObjectRequest["authority"] = jsonObjectRequestHeaders[":authority"];
-        resultObjectRequest["path"] = jsonObjectRequestHeaders[":path"];
-        resultObjectRequest["Action"] = "Request";
-
-        
-
-        string resultJsonRequest = resultObjectRequest.ToString(Formatting.Indented);
-
-        if (firstRequest == false && firstRequestId != resultObjectRequest["requestId"]?.ToString())
+        if (dataToExcelRequest.Count == 0 && dataToExcelResponse.Count == 0)
         {
-            stopSession = true;
-            driver.Quit();
+            firstRequest = true;
         }
         else
         {
-            if (dataToExcelRequest.ContainsKey(resultObjectRequest["requestId"].ToString()))
+            firstRequest = false;
+        }
+
+        if (e.EventName == "requestWillBeSentExtraInfo")
+        {
+            JObject jsonObjectRequest = JObject.Parse(e.EventData.ToString());
+            JObject resultObjectRequest = new JObject();
+            JObject jsonObjectRequestHeaders = JObject.Parse(jsonObjectRequest["headers"].ToString());
+
+            resultObjectRequest["requestId"] = jsonObjectRequest["requestId"];
+            resultObjectRequest["authority"] = jsonObjectRequestHeaders[":authority"];
+            resultObjectRequest["path"] = jsonObjectRequestHeaders[":path"];
+            resultObjectRequest["Action"] = "Request";
+
+            string resultJsonRequest = resultObjectRequest.ToString(Formatting.Indented);
+
+            if (firstRequest == false && firstRequestId != resultObjectRequest["requestId"]?.ToString())
             {
-                dataToExcelRequest[resultObjectRequest["requestId"].ToString()].Add(JObject.Parse(resultJsonRequest));
+                stopSession = true;
+                DataPreprocessing(dataToExcelRequest, dataToExcelResponse);
+                driver.Quit();
+                session.Domains.Network.DisableNetwork();
             }
             else
             {
-                dataToExcelRequest.Add(resultObjectRequest["requestId"].ToString(), new List<JObject> { JObject.Parse(resultJsonRequest) });
+                if (dataToExcelRequest.ContainsKey(resultObjectRequest["requestId"].ToString()))
+                {
+                    dataToExcelRequest[resultObjectRequest["requestId"].ToString()].Add(JObject.Parse(resultJsonRequest));
+                }
+                else
+                {
+                    dataToExcelRequest.Add(resultObjectRequest["requestId"].ToString(), new List<JObject> { JObject.Parse(resultJsonRequest) });
+                }
+
+                // Validate if is the first request
+                if (firstRequest)
+                {
+                    firstRequestId = resultObjectRequest["requestId"].ToString();
+                }
             }
 
-            // Validate if is the first request
-            if (firstRequest)
+            Console.WriteLine("\n------------------------- NEW DATA --------------------------------------------------\n");
+            Console.WriteLine($"\n--------EVENT NAME: ----------------------- {e.EventName}\n");
+            Console.WriteLine(e.EventData);
+            Console.WriteLine("\n------------------------- NEW DATA --------------------------------------------------\n");
+        }
+
+
+        if (e.EventName == "responseReceivedExtraInfo")
+        {
+
+            JObject jsonObjectResponse = JObject.Parse(e.EventData.ToString());
+            JObject resultObjectResponse = new JObject();
+            JObject jsonObjectResponseHeaders = JObject.Parse(jsonObjectResponse["headers"].ToString());
+
+            resultObjectResponse["requestId"] = jsonObjectResponse["requestId"];
+            resultObjectResponse["statusCode"] = jsonObjectResponse["statusCode"];
+            resultObjectResponse["Action"] = "Response";
+
+            string resultJsonResponse = resultObjectResponse.ToString(Formatting.Indented);
+
+            if (dataToExcelResponse.ContainsKey(resultObjectResponse["requestId"].ToString()))
             {
-                firstRequestId = resultObjectRequest["requestId"].ToString();
+                dataToExcelResponse[resultObjectResponse["requestId"].ToString()].Add(JObject.Parse(resultJsonResponse));
             }
+            else
+            {
+                dataToExcelResponse.Add(resultObjectResponse["requestId"].ToString(), new List<JObject> { JObject.Parse(resultJsonResponse) });
+            }
+
+            Console.WriteLine("\n------------------------- NEW DATA --------------------------------------------------\n");
+            Console.WriteLine($"\n--------EVENT NAME: ----------------------- {e.EventName}\n");
+            Console.WriteLine(e.EventData);
+            Console.WriteLine("\n------------------------- NEW DATA --------------------------------------------------\n");
         }
 
+    };
+
+    driver.Navigate().GoToUrl(url);
+}
 
 
 
 
-        Console.WriteLine("\n------------------------- NEW DATA --------------------------------------------------\n");
-        Console.WriteLine($"\n--------EVENT NAME: ----------------------- {e.EventName}\n");
-        Console.WriteLine(e.EventData);
-        Console.WriteLine("\n------------------------- NEW DATA --------------------------------------------------\n");
-    }
 
 
-    if (e.EventName == "responseReceivedExtraInfo")
-    {
-
-        JObject jsonObjectResponse = JObject.Parse(e.EventData.ToString());
-        JObject resultObjectResponse = new JObject();
-        JObject jsonObjectResponseHeaders = JObject.Parse(jsonObjectResponse["headers"].ToString());
-
-        resultObjectResponse["requestId"] = jsonObjectResponse["requestId"];
-        resultObjectResponse["statusCode"] = jsonObjectResponse["statusCode"];
-        resultObjectResponse["Action"] = "Response";
-
-        string resultJsonResponse = resultObjectResponse.ToString(Formatting.Indented);
-
-        if (dataToExcelResponse.ContainsKey(resultObjectResponse["requestId"].ToString()))
-        {
-            dataToExcelResponse[resultObjectResponse["requestId"].ToString()].Add(JObject.Parse(resultJsonResponse));
-        }
-        else
-        {
-            dataToExcelResponse.Add(resultObjectResponse["requestId"].ToString(), new List<JObject> { JObject.Parse(resultJsonResponse) });
-        }
-
-        Console.WriteLine("\n------------------------- NEW DATA --------------------------------------------------\n");
-        Console.WriteLine($"\n--------EVENT NAME: ----------------------- {e.EventName}\n");
-        Console.WriteLine(e.EventData);
-        Console.WriteLine("\n------------------------- NEW DATA --------------------------------------------------\n");
-    }
-
-};
-
-
-
-driver.Navigate().GoToUrl(url);
-
-
-await Task.Delay(TimeSpan.FromSeconds(10));
+//await Task.Delay(TimeSpan.FromSeconds(10));
 
 /* CREATE NEW DICTIONARY */
 
-
-Dictionary<string, List<JObject>> combinedDictionary = new Dictionary<string, List<JObject>>();
-
-
-HashSet<string> allKeys = new HashSet<string>(dataToExcelRequest.Keys.Concat(dataToExcelResponse.Keys));
-
-foreach (var key in allKeys)
+static void DataPreprocessing(Dictionary<string, List<JObject>> dataToExcelRequest, Dictionary<string, List<JObject>>  dataToExcelResponse)
 {
-    List<JObject> requestJObjects = dataToExcelRequest.ContainsKey(key) ? dataToExcelRequest[key] : new List<JObject>();
+    Dictionary<string, List<JObject>> combinedDictionary = new Dictionary<string, List<JObject>>();
 
-    List<JObject> responseJObjects = dataToExcelResponse.ContainsKey(key) ? dataToExcelResponse[key] : new List<JObject>();
 
-    List<JObject> combinedJObjects = InterleaveLists(requestJObjects, responseJObjects);
+    HashSet<string> allKeys = new HashSet<string>(dataToExcelRequest.Keys.Concat(dataToExcelResponse.Keys));
 
-    combinedDictionary.Add(key, combinedJObjects);
-}
-
-foreach (var kvp in combinedDictionary)
-{
-    Console.WriteLine($"Clave: {kvp.Key}");
-
-    foreach (var jObject in kvp.Value)
+    foreach (var key in allKeys)
     {
-        Console.WriteLine(jObject.ToString());
+        List<JObject> requestJObjects = dataToExcelRequest.ContainsKey(key) ? dataToExcelRequest[key] : new List<JObject>();
+
+        List<JObject> responseJObjects = dataToExcelResponse.ContainsKey(key) ? dataToExcelResponse[key] : new List<JObject>();
+
+        List<JObject> combinedJObjects = InterleaveLists(requestJObjects, responseJObjects);
+
+        combinedDictionary.Add(key, combinedJObjects);
     }
+
+    foreach (var kvp in combinedDictionary)
+    {
+        Console.WriteLine($"Clave: {kvp.Key}");
+
+        foreach (var jObject in kvp.Value)
+        {
+            Console.WriteLine(jObject.ToString());
+        }
+    }
+
+    string csvFilePath = "output.csv";
+
+    WriteDictionaryToCsv(combinedDictionary, csvFilePath);
 }
 
-string csvFilePath = "output.csv";
-
-WriteDictionaryToCsv(combinedDictionary, csvFilePath);
 
 static List<JObject> InterleaveLists(List<JObject> list1, List<JObject> list2)
 {
@@ -197,7 +205,7 @@ static List<JObject> InterleaveLists(List<JObject> list1, List<JObject> list2)
     return interleavedList;
 }
 
-static void WriteDictionaryToCsv(Dictionary<string, List<JObject>> dictionary, string filePath)
+static async void WriteDictionaryToCsv(Dictionary<string, List<JObject>> dictionary, string filePath)
 {
     var csvConfig = new CsvConfiguration(CultureInfo.InvariantCulture)
     {
@@ -205,35 +213,43 @@ static void WriteDictionaryToCsv(Dictionary<string, List<JObject>> dictionary, s
         Delimiter = ","
     };
 
-    using (var writer = new StreamWriter(filePath))
-    using (var csv = new CsvWriter(writer, csvConfig))
-    {
-        var allKeys = dictionary.Keys.ToList();
+    //using (var writer = new StreamWriter(filePath)) 
+    //using (var csv = new CsvWriter(writer, csvConfig))
+    //{
+    //    //bool fileExists = File.Exists(filePath);
+    //    var fileExists = false;
 
-        csv.WriteField("Request Id");
-        csv.WriteField("Action");
-        csv.WriteField("URL");
-        csv.WriteField("Status Code");
-        csv.NextRecord();
+    //    if (!fileExists)
+    //    {
+    //        csv.WriteField("Request Id");
+    //        csv.WriteField("Action");
+    //        csv.WriteField("URL");
+    //        csv.WriteField("Status Code");
+    //        csv.NextRecord();
+    //    }
 
-        foreach (var key in allKeys)
-        {
-            var jObjects = dictionary[key];
+    //    var allKeys = dictionary.Keys.ToList();
 
-            foreach (var jObject in jObjects)
-            {
-                var action = jObject["Action"].ToString();
-                var url = action == "Request" ? $"{jObject["authority"]}{jObject["path"]}" : "";
-                var statusCode = action == "Response" ? jObject["statusCode"]?.ToString() : "";
+    //    foreach (var key in allKeys)
+    //    {
+    //        var jObjects = dictionary[key];
 
-                csv.WriteField(key);
-                csv.WriteField(action);
-                csv.WriteField(url);
-                csv.WriteField(statusCode);
-                csv.NextRecord();
-            }
-        }
-    }
+    //        foreach (var jObject in jObjects)
+    //        {
+    //            var action = jObject["Action"].ToString();
+    //            var url = action == "Request" ? $"{jObject["authority"]}{jObject["path"]}" : "";
+    //            var statusCode = action == "Response" ? jObject["statusCode"]?.ToString() : "";
+
+    //            csv.WriteField(key);
+    //            csv.WriteField(action);
+    //            csv.WriteField(url);
+    //            csv.WriteField(statusCode);
+    //            csv.NextRecord();
+    //        }
+    //    }
+    //}
+    
+
 }
 
 Console.ReadLine();
